71108fdd922511e97e21321f92506f34ab6e8241
jcope <jcope@e1z3r5p24.42.us.org>
2018-10-26 10:55:01 -0700

    adding main test

---
+++ LibftWorkFiles/main.c
@@ -0,0 +1,85 @@
+// This file is for testing only.
+
+#define KGRN  "\x1B[32m" //Make text Green.
+#define KRED  "\x1B[31m" //Text Red
+#define KCYN  "\x1B[36m"
+
+#include "../libft/libft.h" //My libft header file.
+#include <stdio.h> //Printf for testing.
+
+int main(void){
+printf("%s ===========Part 1 Function TESTS=========== %s \n", KCYN, KGRN);
+// ft_memset
+// ft_bzero
+// ft_memcpy
+// ft_memccpy
+// ft_memmove
+// ft_memchr
+// ft_memcmp
+// ft_strlen
+// ft_strdup
+// ft_strcpy
+// ft_strncpy
+// ft_strcat
+// ft_strncat
+// ft_strlcat
+// ft_strchr
+// ft_strrchr
+// ft_strstr
+// ft_strnstr
+// ft_strcmp
+// ft_strncmp
+// ft_atoi
+// ft_isalpha
+// ft_isdigit
+// ft_salnum
+// ft_isascii
+// ft_isprint
+// ft_toupper
+// ft_tolower
+
+printf("%s ===========Part 2 Function TESTS=========== %s \n", KCYN, KGRN);
+
+// ft_memalloc
+// ft_memdel
+// ft_strnew
+// ft_strdel
+// ft_strclr
+// ft_striter
+// ft_striteri
+// ft_strmap
+// ft_strmapi
+// ft_strequ
+// ft_strnequ
+// ft_strsub
+// ft_strjoin
+// ft_strtrim
+// ft_strsplit
+// ft_itoa
+// ft_putchar
+
+	ft_putstr("Testing ft_putstr.\n");
+
+// ft_putendl
+
+	printf(" Testing ft_putnbr.");
+	ft_putnbr(34523452);
+	printf("\n");
+
+// ft_putchar_fd
+// ft_putstr_fd
+// ft_putendl_fd
+// ft_putnbr_fd
+
+
+printf("%s ===========Part 3 Function TESTS=========== %s \n", KCYN, KGRN);
+
+// ft_lstnew
+// ft_lstdelone
+// ft_lstdel
+// ft_lstadd
+// ft_lstiter
+// ft_lstmap
+
+	return(0);
+}
