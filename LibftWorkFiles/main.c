cae3e7219306ee9151a342294d4aee0f3e7b9c9d
jcope <jcope@e1z2r6p11.42.us.org>
2018-10-29 23:06:25 -0700

    More functions, backing up

--- LibftWorkFiles/main.c
+++ LibftWorkFiles/main.c
@@ -1,22 +1,39 @@
 // This file is for testing only.
 
 #define KGRN  "\x1B[32m" //Make text Green.
-#define KRED  "\x1B[31m" //Text Red
-#define KCYN  "\x1B[36m"
+#define KRED  "\x1B[31m" //Red
+#define KCYN  "\x1B[36m" //Cyan
 
 #include "../libft/libft.h" //My libft header file.
 #include <stdio.h> //Printf for testing.
-
+#include <string.h>//libc memset for test.
+#include <strings.h>//libc bzero for test.
+#include <ctype.h>//libc: isdigit
 int main(void){
 setbuf(stdout, NULL); // set all writes to stdout to be unbuffered. This helps keep the printfs in correct order.
 
-printf("\n%s ===========Part 1 Function TESTS=========== %s \n", KCYN, KGRN);
+printf("\n%s ======================Part 1 Function TESTS====================== %s", KCYN, KGRN);
 printf("\n\n=====Testing 1 - ft_memset=====");
 	char memset_str1[] = "Hello world";
-	printf("\n		LIBC: %s\n", memset(memset_str1, 'A', 8));
-	printf("\n 		MINE: %s\n", ft_memset(memset_str1, 'A', 8));
+	char memset_str2[] = "Hello world";
+	printf("\n		LIBC: %s", memset(memset_str1, 'A', 8));
+	printf("\n 		MINE: %s", ft_memset(memset_str2, 'A', 8));
 
-// 2 ft_bzero
+printf("\n\n=====Testing 2 - ft_bzero=====");
+	char ft_bzero_str1[] = "My names Jeff";
+	char ft_bzero_str2[] = "My names Jeff";
+	bzero(ft_bzero_str1, 4);
+	ft_bzero(ft_bzero_str2, 4);
+	printf("\n		LIBC: %s", ft_bzero_str1);
+	printf("\n 		MINE: %s", ft_bzero_str2);
+	
+	char ft_bzero_str3[] = "My names Jeff";
+	char ft_bzero_str4[] = "My names Jeff";
+	bzero(ft_bzero_str3, 0);
+	ft_bzero(ft_bzero_str4, 0);
+	printf("\n		LIBC: %s", ft_bzero_str3);
+	printf("\n 		MINE: %s", ft_bzero_str4);
+
 // 3 ft_memcpy
 // 4 ft_memccpy
 // 5 ft_memmove
@@ -32,14 +49,14 @@ printf("\n\n=====Testing 9 - ft_strdup=====");
 	printf("\n 		MINE: %s", ft_strdup("Stumpy is cool!"));
 
 printf("\n\n=====Testing 10 - ft_strcpy=====");
-	char ft_strcpy_dst[15];
+	char ft_strcpy_dst[16];
 	printf("\n		LIBC: %s", strcpy(ft_strcpy_dst, "Stumpy is cool!"));
 	printf("\n 		MINE: %s", ft_strcpy(ft_strcpy_dst, "Stumpy is cool!"));
 
 printf("\n\n=====Testing 11 - ft_strncpy=====");
-	char ft_strncpy_dst[15];
-	printf("\n		LIBC: %s\n", strncpy(ft_strncpy_dst, "Stumpy is cool!", 6));
-	printf("\n 		MINE: %s\n", ft_strncpy(ft_strncpy_dst, "Stumpy is cool!", 6));
+	char ft_strncpy_dst[16];
+	printf("\n		LIBC: %s", strncpy(ft_strncpy_dst, "Stumpy is cool!", 6));
+	printf("\n 		MINE: %s", ft_strncpy(ft_strncpy_dst, "Stumpy is cool!", 6));
 
 // 12 ft_strcat
 // 13 ft_strncat
@@ -47,29 +64,29 @@ printf("\n\n=====Testing 11 - ft_strncpy=====");
 
 printf("\n\n=====Testing 15 - ft_strchr=====");
 	char strchr_str1[] = "Yellow Butterfly";
-	printf("\n		EXPECT: llow Butterfly");
-	printf("\n 		RESULT: %s\n", ft_strchr(strchr_str1,'l'));
+	printf("\n 		LIBC: %s", strchr(strchr_str1,'l'));
+	printf("\n 		MINE: %s", ft_strchr(strchr_str1,'l'));
 
 	char strchr_str2[] = "Snap Crackle Pop"; 
-	printf("\n		EXPECT: p Crackle Pop");
-	printf("\n 		RESULT: %s\n", ft_strchr(strchr_str2,'p'));
+	printf("\n 		LIBC: %s", strchr(strchr_str2,'p'));
+	printf("\n 		MINE: %s", ft_strchr(strchr_str2,'p'));
 
 printf("\n\n=====Testing 16 - ft_strrchr=====");
 	char strrchr_str1[] = "Yellow Butterfly"; 
-	printf("\n		EXPECT: ly");
-	printf("\n 		RESULT: %s\n", ft_strrchr(strrchr_str1,'l'));
+	printf("\n 		LIBC: %s", strrchr(strrchr_str1,'l'));
+	printf("\n 		MINE: %s", ft_strrchr(strrchr_str1,'l'));
 
 	char strrchr_str2[] = "A string without n"; 
-	printf("\n		EXPECT: (null)");
-	printf("\n 		RESULT: %s\n", ft_strrchr(strrchr_str2,'l'));
+	printf("\n 		LIBC: %s", strrchr(strrchr_str2,'l'));
+	printf("\n 		MINE: %s", ft_strrchr(strrchr_str2,'l'));
 
 	char strrchr_str3[] = "l is the first char"; 
-	printf("\n		EXPECT: l is the first char");
-	printf("\n 		RESULT: %s\n", ft_strrchr(strrchr_str3,'l'));
+	printf("\n 		LIBC: %s", strrchr(strrchr_str3,'l'));
+	printf("\n 		MINE: %s", ft_strrchr(strrchr_str3,'l'));
 
 	char strrchr_str4[] = "the last char is l";
-	printf("\n		EXPECT: l");
-	printf("\n 		RESULT: %s\n", ft_strrchr(strrchr_str4,'l'));
+	printf("\n 		LIBC: %s", strrchr(strrchr_str4,'l'));
+	printf("\n 		MINE: %s", ft_strrchr(strrchr_str4,'l'));
 
 
 // 17 ft_strstr
@@ -78,35 +95,69 @@ printf("\n\n=====Testing 16 - ft_strrchr=====");
 // 20 ft_strncmp
 
 printf("\n\n=====Testing 21 - ft_atoi=====");
-	printf("\n		EXPECT: %d", atoi("HelloWorld"));
-	printf("\n 		RESULT: %d", ft_atoi("HelloWorld"));
-	//todo but getting Error 1 in terminal
+	printf("\n		LIBC: %d", atoi("HelloWorld"));
+	printf("\n 		MINE: %d", ft_atoi("HelloWorld"));
 
+printf("\n\n=====Testing 22 - ft_isalpha=====");
+	printf("\n 		LIBC: %d", isalpha('a'));
+	printf("\n 		MINE: %d\n", ft_isalpha('a'));
 
-// 22 ft_isalpha
+	printf("\n 		LIBC: %d", isalpha('!'));
+	printf("\n 		MINE: %d\n", ft_isalpha('!'));
 
 printf("\n\n=====Testing 23 - ft_isdigit=====");
-	printf("\n		EXPECT: 1");
-	printf("\n 		RESULT: %d\n", ft_isdigit(8));
+	printf("\n 		LIBC: %d", isdigit(8));
+	printf("\n 		MINE: %d\n", ft_isdigit(8));
 
-	printf("\n		EXPECT: 0");
-	printf("\n 		RESULT: %d\n", ft_isdigit('a'));
+	printf("\n 		LIBC: %d", isdigit('a'));
+	printf("\n 		MINE: %d\n", ft_isdigit('a'));
 
-	printf("\n		EXPECT: 0");
-	printf("\n 		RESULT: %d\n", ft_isdigit(99));	
+	printf("\n 		LIBC: %d", isdigit(99));	
+	printf("\n 		MINE: %d\n", ft_isdigit(99));	
 
-// 24 ft_salnum
-// 25 ft_isascii
-// 26 ft_isprint
-printf("\n\n=====Testing 27 - ft_toupper=====");
-	printf("\n		EXPECT: Z");
-	printf("\n 		RESULT: %c", ft_toupper('z'));
+	printf("\n 		LIBC: %d", isdigit('8'));
+	printf("\n 		MINE: %d\n", ft_isdigit('8'));
+
+printf("\n\n=====Testing 24 - ft_isalnum=====");
+	printf("\n 		LIBC: %d", isalnum('a'));
+	printf("\n 		MINE: %d\n", ft_isalnum('a'));
+
+	printf("\n 		LIBC: %d", isalnum('!'));
+	printf("\n 		MINE: %d\n", ft_isalnum('!'));
+
+	printf("\n 		LIBC: %d", isalnum('8'));
+	printf("\n 		MINE: %d\n", ft_isalnum('8'));		
+
+	printf("\n 		LIBC: %d", isalnum(99));
+	printf("\n 		MINE: %d\n", ft_isalnum(99));	
+
+printf("\n\n=====Testing 25 - ft_isascii=====");
+	printf("\n 		LIBC: %d", isascii('a'));
+	printf("\n 		MINE: %d\n", ft_isascii('a'));
+
+	printf("\n 		LIBC: %d", isascii('#'));
+	printf("\n 		MINE: %d\n", ft_isascii('#'));
+
+printf("\n=====Testing 26 - ft_isprint=====");
+	printf("\n 		LIBC: %d", isprint('z'));
+	printf("\n 		MINE: %d", ft_isprint('z'));
+
+	printf("\n 		LIBC: %d", isprint(' '));
+	printf("\n 		MINE: %d", ft_isprint(' '));
+
+	printf("\n 		LIBC: %d", isprint('#'));
+	printf("\n 		MINE: %d", ft_isprint('#'));
+
+
+printf("\n=====Testing 27 - ft_toupper=====");
+	printf("\n 		LIBC: %c", toupper('z'));
+	printf("\n 		MINE: %c", ft_toupper('z'));
 
 printf("\n\n=====Testing 28 - ft_tolower=====");
-	printf("\n		EXPECT: z");
-	printf("\n 		RESULT: %c", ft_tolower('Z'));	
+	printf("\n 		LIBC: %c", tolower('Z'));
+	printf("\n 		MINE: %c", ft_tolower('Z'));	
 
-printf("\n%s ===========Part 2 Function TESTS=========== %s \n", KCYN, KGRN);
+printf("\n%s ======================Part 2 Function TESTS====================== %s", KCYN, KGRN);
 
 // 29 ft_memalloc
 // 30 ft_memdel
@@ -149,7 +200,7 @@ printf("\n\n=====Testing 48 - ft_putnbr=====");
 // 52 ft_putnbr_fd
 
 
-printf("\n%s ===========Part 3 Function TESTS=========== %s \n", KCYN, KGRN);
+printf("\n%s ======================Part 3 Function TESTS====================== %s \n", KCYN, KGRN);
 
 // 53 ft_lstnew
 // 54 ft_lstdelone
