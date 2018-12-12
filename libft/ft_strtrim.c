1b311bf9f26da6ee9020fc16c7ded4df28955183
jcope <thejemmeh@gmail.com>
2018-12-12 14:23:49 -0800

    strtrim working with commented out code.

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -12,29 +12,45 @@
 
 #include "libft.h"
 
-char			*ft_strtrim(char const *s)
+char	*ft_strtrim(char const *s)
 {
-	char	*new;
-	int		i;
-	int		len;
+	char const *end;
 
 	if (!s)
 		return (NULL);
-	len = ft_strlen(s);
-	i = ft_strlen(s);
-	while(ft_isspace(s[i]))
-		i--;
-	len -= ft_strlen(s) - i;
-	i = 0;
-	while(ft_isspace(s[i]))
-		i++;
-	len -= i;
-	if(!(new = ft_strnew(len)))
- 		return (NULL);
- 	ft_strncpy(new, s + i, len);
- 	new[ft_strlen(new)] = '\0';
-	return(new);
+	while (ft_isspace(*s))
+		s++;
+	if (*s == '\0')
+		return (ft_strnew(0));
+	end = s + ft_strlen(s) - 1;
+	while (ft_isspace(*end))
+		end--;
+	return (ft_strsub(s, 0, end - s + 1));
 }
+
+// char			*ft_strtrim(char const *s)
+// {
+// 	char	*new;
+// 	size_t	i;
+// 	int		len;
+// 
+// 	if (!s)
+// 		return (NULL);
+// 	len = ft_strlen(s);
+// 	i = ft_strlen(s);
+// 	while((s[i]) && i > 0)
+// 		i--;
+// 	len = len - (ft_strlen(s) - i);
+// 	i = 0;
+// 	while(ft_isspace(s[i]) && i < ft_strlen(s))
+// 		i++;
+// 	len -= i;
+// 	if(!(new = ft_strnew(len)))
+//  		return (NULL);
+//  	ft_strncpy(new, s + i, len);
+//  	new[ft_strlen(new)] = '\0';
+// 	return(new);
+// }
 
 // char			*ft_strtrim(char const *s)
 // {
--- libft/libft.h
+++ libft/libft.h
@@ -23,6 +23,7 @@ typedef struct	s_list
 	struct s_list	*next;
 }				t_list;
 
+int				ft_isspace(char c);
 size_t			ft_strlen(const char *s);
 char			*ft_strcpy(char *dst, const char *src);
 char			*ft_strncpy(char *dst, const char *src, size_t len);
@@ -81,6 +82,6 @@ void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstadd(t_list **alst, t_list *new);
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
-int	ft_isspace(char c);
+
 
 #endif
